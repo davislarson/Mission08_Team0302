59d779cbbfbb8515a8e8039e715e806a671fc248
danerjenkins <danej7@byu.edu>
2025-02-26 15:10:16 -0700

    db setup

---
+++ Mission08_Team0302/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team0302.Models
+{
+    public class Task
+    {
+        [Key]
+        public string task_id { get; set; }
+        public string task_description { get; set; }
+        public string due_date { get; set; }
+        [Range(1, 4)]
+        public int quadrant { get; set; }
+        public int category_id { get; set; }
+        public Category Category { get; set; }
+        public bool completed { get; set; }
+
+    }
+}

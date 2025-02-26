434650224e4f3b48e03b44d38e7becfd60d16741
danerjenkins <danej7@byu.edu>
2025-02-26 15:15:30 -0700

    fix db setup

--- Mission08_Team0302/Models/Task.cs
+++ Mission08_Team0302/Models/Task.cs
@@ -7,10 +7,13 @@ namespace Mission08_Team0302.Models
     {
         [Key]
         public string task_id { get; set; }
+        [Required]
         public string task_description { get; set; }
         public string due_date { get; set; }
         [Range(1, 4)]
+        [Required]
         public int quadrant { get; set; }
+        [Required]
         public int category_id { get; set; }
         public Category Category { get; set; }
         public bool completed { get; set; }
